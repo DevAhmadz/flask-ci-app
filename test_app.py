5b387c820221ed37735f55d79e2e57a6a3546956
DevAhmadz <54114052+DevAhmadz@users.noreply.github.com>
2025-05-20 23:52:06 +0000

    fix: corrected test_client typo

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ from app import app
 
 
 def test_home():
-  client = app_test_client()
+  client = app.test_client()
   response = client.get('/')
   assert response.status_code == 200
   assert b"Hello DevOps!" in response.data

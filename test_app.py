fa611cb3b00484148dc3371ad312c59417cde00e
DevAhmadz <54114052+DevAhmadz@users.noreply.github.com>
2025-05-20 23:19:11 +0000

    fix syntax error in test

--- test_app.py
+++ test_app.py
@@ -1,9 +1,9 @@
 from flask import Response
 from app import app
 
+
 def test_home():
   client = app_test_client()
   response = client.get('/')
   assert response.status_code == 200
-  assert b"Hello DevOps!" in" response.data
-  
+  assert b"Hello DevOps!" in response.data
